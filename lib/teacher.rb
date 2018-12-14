12f6b9f856a58a9bdc73b1e13159e5e04d19c7d6
Tara Sabella <tarasabella@gmail.com>
2018-12-14 18:29:52 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,7 +2,11 @@ class Teacher <
   User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-end 
+  
 def teach # => returns a random element of the array 
+KNOWLEDGE.sample
 
-end
+# n.sample outputs a new array with a random element 
+  end 
+ 
+end 
--- lib/user.rb
+++ lib/user.rb
@@ -4,20 +4,22 @@ class User
   attr_reader :first_name, :last_name
   attr_writer :first_name, :last_name
   
+end 
+  
   #create both a getter and setter so that first and last name can be accessed and manipulated 
   
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name 
-  end 
+#   def initialize(first_name, last_name)
+#     @first_name = first_name
+#     @last_name = last_name 
+#   end 
  
-  #for every new object instance created, the initialzs arguments will be passed to it 
-  #every istance objectc of user must have a first & last name upon initialization 
-  def first_name 
-    self.first_name
-  end 
-  def last_name
-   self.last_name 
-  end 
-end 
-end 
+#   #for every new object instance created, the initialzs arguments will be passed to it 
+#   #every istance objectc of user must have a first & last name upon initialization 
+#   def first_name 
+#     self.first_name
+#   end 
+#   def last_name
+#   self.last_name 
+#   end 
+# end 
+# end 
