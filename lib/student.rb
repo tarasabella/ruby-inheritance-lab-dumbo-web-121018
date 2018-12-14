b2c2239567521c6295de636098da1b955e1694aa
Tara Sabella <tarasabella@gmail.com>
2018-12-14 18:16:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,14 @@
 class Student < 
   User
+attr_accessor :Knowledge
 
+def initialize
+  @knowledge = []
 end
+def learn(knowledge)
+  @knowledge << knowledge
+end 
+def knowledge
+  return @knowledge
+end 
+end 
